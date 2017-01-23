756b91f7877bdbef570094002970e30c0f5f3eaf
Andrew Bagwell <andrewbagwell@Andrews-MBP.attlocal.net>
2017-01-23 11:59:50 -0600

    reduce synonyms to those confined to game

--- commandNLP.py
+++ commandNLP.py
@@ -7,17 +7,22 @@ import re
 class commandNLP:
 
 
-	'''
-	class variables
+
 
 	'''
-	cwd = os.getcwd()
-	gameVerbs = ["look", "take", "help", "inventory", "use", 
-	"drop", "eat", "drink", "pull", "hit", "put", 
-	"savegame", "loadgame", "push", "wield", "wear"]
-	verbPrepositionCombos = {}
-	synonymFile = cwd + "/synonymFile.txt"
-	synonymsDictionary = {}
+
+	Definition: This is the constuctor for the commandNLP 
+
+	'''
+
+	def __init__(self):
+
+		self.cwd = os.getcwd()
+		self.gameVerbs = ["look", "take", "help", "inventory", "use", 
+		"drop", "eat", "drink", "pull", "hit", "put", 
+		"savegame", "loadgame", "push", "wield", "wear"]
+		self.verbPrepositionCombos = {}
+		self.synonymsDictionary = {}
 
 
 	'''
@@ -26,7 +31,18 @@ class commandNLP:
 
 	'''
 
-	def __init__(self, synonymFile):
+	def buildSynonymDict(self):
+
+		synonymFilePath = cwd + "/synonymFile.txt" #file source: https://justenglish.me/2014/04/18/synonyms-for-the-96-most-commonly-used-words-in-english/
+
+		with open(synonymFilePath) as f:
+			for line in f:
+				splitLine = line.split("—")
+				verb, synonymList = splitLine
+				parsedSynonyms = synonymList.split(",")
+				for word in parsedSynonyms:
+					self.synonyms
+
 
 
 
@@ -37,10 +53,15 @@ class commandNLP:
 	'''
 
 	def parseCommand(commandString):
-		foobar= 3
 
 
 
 
+def doNLP():
+	potentialCommand = input("Your move: ")
+
+
+if __name__ =='__main__':
+	doNLP()
 
 
